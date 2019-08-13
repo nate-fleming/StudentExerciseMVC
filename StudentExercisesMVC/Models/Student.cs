a8a84c464fc2c0d5d5b0dcb493ef2696e61fb81f
Nate Fleming <nfleming4@gmail.com>
2019-08-13 07:04:54 -0700

    add start

---
+++ StudentExercisesMVC/Models/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercisesMVC.Models
+{
+    public class Student
+    {
+        [Required]
+        [Display(Name = "Student Id")]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Slack Handle")]
+        public string SlackHandle { get; set; }
+
+        [Required]
+        [Display(Name = "Cohort Id")]
+        public int CohortId { get; set; }
+
+
+        //Computed Property
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
